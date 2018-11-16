dfe1c45bb65c0fb2d70bf19de98b55b2ab4e2de8
yanntoque <toqueyann@gmail.com>
2018-11-16 10:55:41 +0100

    Ajout des classes client et listeclients

---
+++ src/java/Beans/Client.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author Yann Toqué
+ * @author Valentin Lecouple
+ */
+public class Client {
+    
+    private String nom;
+    private String prenom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+
+}
