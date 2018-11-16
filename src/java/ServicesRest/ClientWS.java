6b94d19c29240f3411570d43516c0f5063a8c4d4
vlcpl <valentin.lecouple@gmail.com>
2018-11-16 11:43:24 +0100

    Ajout du pathparam dans les parametres du put

--- src/java/ServicesRest/ClientWS.java
+++ src/java/ServicesRest/ClientWS.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -49,11 +50,12 @@ public class ClientWS {
 
     /**
      * PUT method for updating or creating an instance of ClientWS
-     * @param content representation for the resource
+     * @param nom
+     * @param prenom
      */
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
-    public void putNewClient(String nom, String prenom) {
+    public void putNewClient(@PathParam("nom") String nom, @PathParam("prenom") String prenom) {
         Client newClient = new Client(nom, prenom);
         this.lstClients.ajouterClientDansListe(newClient);
     }
